ebb60fc411d97877836a1c5f0eb4cd2eda71d48c
abap34 <abap0002@gmail.com>
2024-07-05 00:23:38 +0900

    全体のスライドもビルドするようにした

--- build.py
+++ build.py
@@ -1,16 +1,55 @@
 import os
 import glob
 
-files = glob.glob('**/*.md', recursive=True)
+pages = glob.glob('**/*.md', recursive=True)
 
 def isexcluded(file):
     return file.startswith('2023/') or file.endswith('lecture.md')
 
-files = [file for file in files if not isexcluded(file)]
+pages = [page for page in pages if not isexcluded(page)]
 
-for file in files:
-    basename = file.split('.')[0]
-    cmd = f"almo-cli build {file} -o {basename}.html --config config.yaml"
+for page in pages:
+    basename = page.split('.')[0]
+    cmd = f"almo-cli build {page} -o {basename}.html --config config.yaml"
     print(cmd)
     os.system(cmd)
 
+
+slides = glob.glob('ch*/lecture.md', recursive=True)
+imgs = glob.glob('ch*/img/*', recursive=True)
+
+# すべての画像にチャンネル名の prefix をつけて img/ にコピー
+os.makedirs('img', exist_ok=True)
+
+for img in imgs:
+    ch_prefix = img.split('/')[0]
+    cmd = f'cp {img} img/{ch_prefix}_{img.split("/")[-1]}'
+    print(cmd)
+    os.system(cmd)
+
+slides.sort()
+
+def remove_frontmatter(content):
+    lines = content.split('---')
+    return '---' + '---'.join(lines[2:])
+    
+def replace_img_path(ch_prefix, content):
+    # ![alt](img/... -> ![alt](img/chXX_...
+    return content.replace('img/', f'img/{ch_prefix}_')
+
+with open('slides.md', 'w') as f:
+    # 先頭の frontyaml だけ残す
+    with open(slides[0]) as f1:
+        f.write(replace_img_path(slides[0].split('/')[0], f1.read()))
+
+
+
+    for slide in slides[1:]:
+        ch_prefix = slide.split('/')[0]
+        with open(slide) as f1:
+            f.write('\n\n<!-- PAGE BREAK -->\n\n')
+            f.write(f'\n\n<!-- SLIDE: {slide} -->\n\n')
+            content = f1.read()
+            content = remove_frontmatter(content)
+            content = replace_img_path(ch_prefix, content)
+            f.write(content)
